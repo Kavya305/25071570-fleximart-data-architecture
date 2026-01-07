395e436ae36b19e2886f097e0cad98bfcb48edf3
Ksingal30 <kavya.singal30@gmail.com>
2026-01-07 20:19:16 +0530

    progress

--- part1-database-etl/etl_pipeline.py
+++ part1-database-etl/etl_pipeline.py
@@ -19,20 +19,21 @@ sales = pd.read_csv("data/sales_raw.csv")
 # Customers
 customers.drop_duplicates(inplace=True)
 customers.dropna(subset=["email"], inplace=True)
+customers["phone"] = customers["phone"].astype(str)
+customers["phone"] = customers["phone"].str.replace(r"\D", "", regex=True)
+customers["phone"] = "+91-" + customers["phone"]
 
-# customers["phone"] = customers["phone"].str.replace(r"\D", "", regex=True)
-# customers["phone"] = "+91-" + customers["phone"]
+# Products
+products.drop_duplicates(inplace=True)
+products.dropna(subset=["price"], inplace=True)
+products["category"] = products["category"].astype(str)
+products["category"] = products["category"].str.strip()
+products["category"] = products["category"].str.title()
+products["stock_quantity"] = products["stock_quantity"].fillna(0).astype(int)
 
-# # Products
-# products.drop_duplicates(inplace=True)
-# products.dropna(subset=["price"], inplace=True)
-# products["stock_quantity"].fillna(0, inplace=True)
-# products["category"] = products["category"].str.title()
+# Sales
+sales.drop_duplicates(inplace=True)
+sales["transaction_date"] = pd.to_datetime(sales["transaction_date"], errors="coerce")
+sales.dropna(subset=["customer_id", "product_id", "transaction_date"], inplace=True)
 
-# # Sales
-# sales.drop_duplicates(inplace=True)
-# sales["order_date"] = pd.to_datetime(sales["order_date"], errors="coerce")
-# sales.dropna(subset=["customer_id", "product_id", "order_date"], inplace=True)
 
-# # ------------------ LOAD ------------------
-# # (We will insert step-by-step in next message)
